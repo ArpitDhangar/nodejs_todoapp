b84e2be470a5acc4290634b90582cf9cb8dd4b9d
Arpit Dhangar <arpitdhangar53741@gmail.com>
2024-11-10 08:03:29 +0530

    Intial Commit

---
+++ middlewares/error.js
@@ -0,0 +1,19 @@
+class ErrorHandler extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal server Error";
+  console.log(err.statusCode);
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    success: true,
+    message: err.message,
+  });
+};
+
+export default ErrorHandler;
